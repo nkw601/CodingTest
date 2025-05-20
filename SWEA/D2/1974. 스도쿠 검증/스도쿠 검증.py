4336a70ba5086c786331148bb53c847275c9a50c
Lani <144819031+kw601@users.noreply.github.com>
2025-05-20 18:05:46 +0900

    [D2] Title: 스도쿠 검증, Time: 61 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,11 +1,3 @@
-# import sys
-# data = sys.stdin.read().splitlines()
-# T = int(data[0])
-# sudoku = []
-# for line in data[1:]:
-#     sudoku.append(list(map(int, line.split())))
-
-
 T = int(input())
 for test_case in range(1, T + 1):
     sudoku = []
@@ -15,63 +7,27 @@ for test_case in range(1, T + 1):
 
     # 가로
     for line in sudoku:
-        nums = set()
-        for num in line:
-            if not (1 <= num <= 9):
-                available = False
-                break
-            if num in nums:
-                available = False
-                break
-            else:
-                nums.add(num)
+        nums = set(line)
+        if len(nums) != 9:
+            available = False
+
     # 세로
-    columns = []
-    for j in range(9):
-        col = [sudoku[i][j] for i in range(9)]
-        columns.append(col)
-
-    for col in columns:
-        if not available:
-            break
-
-        nums = set()
-        for num in col:
-            if not (1 <= num <= 9):
-                available = False
-                break
-            if num in nums:
-                available = False
-                break
-            else:
-                nums.add(num)
-    # 네모
-
-    rectangles = []
-    for i in range(0, 9, 3):  # 시작 행: 0, 3, 6
-        for j in range(0, 9, 3):  # 시작 열: 0, 3, 6
-            block = []
-            for dx in range(3):
-                for dy in range(3):
-                    block.append(sudoku[i + dx][j + dy])
-                rectangles.append(block)
-
-    for r in rectangles:
-        if not available:
-            break
-
-        nums = set()
-        for num in r:
-            if not (1 <= num <= 9):
-                available = False
-                break
-            if num in nums:
-                available = False
-                break
-            else:
-                nums.add(num)
-
     if available:
-        print(f"#{test_case} 1")
-    else:
-        print(f"#{test_case} 0")
+        for j in range(9):
+            col = set([sudoku[i][j] for i in range(9)])
+            if len(col) != 9:
+                available = False
+                break
+    # 네모
+    if available:
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                block = set()
+                for dx in range(3):
+                    for dy in range(3):
+                        block.add(sudoku[i + dx][j + dy])
+                if len(block) != 9:
+                    available = False
+                    break
+
+    print(f"#{test_case} {1 if available else 0}")
