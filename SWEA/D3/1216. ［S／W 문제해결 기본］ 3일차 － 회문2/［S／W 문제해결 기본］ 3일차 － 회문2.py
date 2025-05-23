e769efaae9fa1851bafc279fe0f703e8ef423b03
Lani <144819031+kw601@users.noreply.github.com>
2025-05-23 18:30:25 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 1,463 ms, Memory: 67,968 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,45 @@
+# 가로, 세로를 모두 보아 가장 긴 회문의 길이
+# letters = ["A", "B", "C"]
+def is_palindrome_col(sent, col, start, length):
+    for i in range(length // 2):
+        if sent[start + i][col] != sent[start + length - 1 - i][col]:
+            return False
+    return True
+
+
+T = 10
+for test_case in range(1, T + 1):
+    n = input()
+    # ['ABCDADCDADCADC', 'ADFADFDSCDCAD', 'ADFADSCDAS']
+    sent = [list(input()) for _ in range(100)]
+    length = 0
+    found = False
+
+    # 가로
+    for length in range(100, 0, -1):
+        for i in range(100):  # 가로 한 줄씩
+            for start in range(0, 100 - length + 1):
+                substr = sent[i][start : start + length]
+                if substr == substr[::-1]:
+                    found = True
+                    break
+            if found:
+                break
+        if found:
+            break
+
+    # 세로
+    found = False
+    length_col = 0
+    for length_col in range(100, 0, -1):
+        for i in range(100):  # 세로 한 줄씩
+            for start in range(0, 100 - length_col + 1):
+                if is_palindrome_col(sent, i, start, length_col):
+                    found = True
+                    break
+            if found:
+                break
+        if found:
+            break
+
+    print(f"#{n} {max(length_col, length)}")
