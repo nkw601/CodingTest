81b595ca2d9cbe5a194e0f9eb02f945fb6a31fc6
Lani <144819031+kw601@users.noreply.github.com>
2025-06-01 18:02:16 +0900

    [level 2] Title: 요격 시스템, Time: 318.38 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,3 @@
 def solution(targets):
-    targets.sort(key=lambda x: x[1])
-    
     answer = 0
-    end = 0
-    for t in targets:
-        if t[0] >= end:
-            answer += 1
-            end = t[1]
-        
     return answer
