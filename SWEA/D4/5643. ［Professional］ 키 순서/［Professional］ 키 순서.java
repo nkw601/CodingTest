fd71e230f1b3e145f36d21261a88f1a5a6dbe3dc
Lani <144819031+nkw601@users.noreply.github.com>
2025-09-11 10:52:21 +0900

    [D4] Title: [Professional] 키 순서, Time: 510 ms, Memory: 97,772 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,59 +2,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
 	static int N, adj[][];
-	static int count; // 자신보다 키가 크거나 작은 학생 수
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int tc = 0; tc < T; tc++) {
+		for(int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			int M = Integer.parseInt(br.readLine());
 			
 			adj = new int [N+1][N+1];
+			// 메모 안된 상태로 초기화
+			for(int i = 1; i < N + 1; i++) adj[i][0] = -1;
+			
 			for(int m =0; m < M; m++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				adj[a][b] = 1; // a가 b보다 키가 큼
 			}
-			int ans = 0;
-			for(int i = 1; i <= N; i++) { // 모든 학생에 대해 탐색
-				count = 0;
-				boolean[] visited = new boolean[N+1];
-				gtDFS(i, visited); // 나보다 큰 학생 수를 count에 저장
-				ltDFS(i, visited); // 나보다 작은 학생 수를 count에 더함
-				
-				if(count == N - 1) {
-					ans++;
-				}
-				
+			
+			for(int i = 1; i <= N; i++) { // 모든 학생에 대해 탐색(이 과정에서 간접 관계를 직접 관계로 경로 압축)
+				if(adj[i][0] == -1) dfs(i);
 			}
-			sb.append("#").append(tc + 1).append(" ").append(ans).append("\n");
+			// 자신보다 작은 학생
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= N; j++) {
+					adj[0][i] += adj[j][i];
+				}
+			}
+		
+			int ans = 0;
+			for(int i = 1; i <= N; i++) {
+				if(adj[i][0] + adj[0][i]== N - 1) ans++;
+			}
+            sb.append("#").append(tc).append(" ").append(ans).append("\n");
+
 		}
 		System.out.println(sb);
 	}
 	
-	private static void gtDFS(int cur, boolean[] visited) {
-		visited[cur] = true;
+	private static void dfs(int cur) {
 		for(int i = 1; i <= N; i++) {
-			if(adj[cur][i]==1 && !visited[i]) {
-				++count;
-				gtDFS(i, visited);
+			if(adj[cur][i]==1 ) { // cur보다 큰 i라면
+				if(adj[i][0] == -1) dfs(i); // i가 탐색을 하지 않았으면 탐색하러 가기
+				
+				// i가 탐색을 이미 해서 내려왔거나, i 탐색을 마치고 내려온 경우
+				if(adj[i][0] > 0) { // i보다 큰 학생이 존재 -> cur < i < J ==> cur < J로 바꾸기
+					for(int j = 0; j <= N; j++) { // i보다 큰 학생 J 찾아서 cur < J 관계로 표현
+						if(adj[i][j] == 1) adj[cur][j] = 1;
+					}
+				}
 			}
 		}
-	}
-	private static void ltDFS(int cur, boolean[] visited) {
-		visited[cur] = true;
+		
+		// 자신보다 큰 학생들 수 카운팅 후 메모
+		int cnt = 0;
 		for(int i = 1; i <= N; i++) {
-			if(adj[i][cur]==1 && !visited[i]) {
-				++count;
-				ltDFS(i, visited);
-			}
+			if(adj[cur][i] > 0) cnt++; // 나보다 큰 애 있으면 cnt++
 		}
+		adj[cur][0] = cnt;
 	}
+
 }
