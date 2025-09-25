c64b157448e9a20d139f0ce90cc786e2d1cc836e
Lani <144819031+nkw601@users.noreply.github.com>
2025-09-25 23:02:51 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		boolean[] students = new boolean[30];
+		
+		for(int i = 0; i < 28; i++) {
+			int n = Integer.parseInt(br.readLine());
+			students[n - 1] = true;
+		}
+		
+		for(int i = 0; i < 30; i++) {
+			if(students[i] == false) System.out.println(i + 1);
+		}
+		
+	}
+}	
