60b1e6ad112f0094aa78fc3f562fc01e801fc575
Lani <144819031+nkw601@users.noreply.github.com>
2025-10-01 14:48:34 +0900

    [Silver V] Title: 올림픽, Time: 96 ms, Memory: 14272 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N, K;
+	private static int[][] medals;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		medals = new int[N + 1][3];
+		for(int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			medals[i][0] = Integer.parseInt(st.nextToken()); // 금
+			medals[i][1] = Integer.parseInt(st.nextToken()); // 은
+			medals[i][2] = Integer.parseInt(st.nextToken()); // 동
+		}
+		int winner = 0;
+		int gold = medals[K][0];
+		int silv = medals[K][1];
+		int bron = medals[K][2];
+
+		for(int i = 1; i <= N; i++) {
+			if(i == K) continue;
+			
+			if(medals[i][0] > gold) winner++;
+			else if(medals[i][0] < gold) continue;
+			else {
+				if(medals[i][1] > silv) winner++;
+				else if(medals[i][1] < silv) continue;
+				else {
+					if(medals[i][2] > bron) winner++;
+					else continue;
+				}
+			}
+		}
+		System.out.println(winner + 1);
+	}
+
+}
