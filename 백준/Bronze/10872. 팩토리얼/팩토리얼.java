55c6ca2a59ecb88a7aedfc559e62eaa23e025d90
Lani <144819031+nkw601@users.noreply.github.com>
2025-10-25 12:05:54 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long fact = 1;
+        for(int i = 1; i <= N; i++){
+            fact *= i;
+        }
+        System.out.println(fact);
+    }
+
+}
