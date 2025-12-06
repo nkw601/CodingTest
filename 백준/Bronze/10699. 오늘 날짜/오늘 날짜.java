f4e556b1ded1241b942c61c455e664908c2354db
Lani <144819031+nkw601@users.noreply.github.com>
2025-12-06 23:38:52 +0900

    [Bronze V] Title: 오늘 날짜, Time: 140 ms, Memory: 16940 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        System.out.println(today);
+    }
+}
