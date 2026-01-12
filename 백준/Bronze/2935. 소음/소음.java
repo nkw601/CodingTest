387c8a1cc5951965a517ce30502a552edc568d19
Lani <144819031+nkw601@users.noreply.github.com>
2026-01-12 15:34:13 +0900

    [Bronze III] Title: 소음, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2935. 소음/소음.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger n1 = new BigInteger(br.readLine());
+        String op = br.readLine();
+        BigInteger n2 = new BigInteger(br.readLine());
+
+        if (op.equals("*")) {
+            System.out.println(n1.multiply(n2));
+        } else {
+            System.out.println(n1.add(n2));
+        }
+    }
+}
