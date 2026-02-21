052ab05d727da66688946e9e935afeaac5da2b8b
Lani <144819031+nkw601@users.noreply.github.com>
2026-02-21 23:29:24 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 14860 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        int ans = 0;
+
+        for (int i = 0; i < N; i++) {
+            int target = numbers[i];
+            int left = 0;
+            int right = N - 1;
+
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+
+                int sum = numbers[left] + numbers[right];
+
+                if (sum == target) {
+                    ans++;
+                    break;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
