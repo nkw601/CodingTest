86a19348d4a63a016c15f84f61f380fdbf3a35cc
Lani <144819031+nkw601@users.noreply.github.com>
2026-04-05 09:28:30 +0900

    [Gold IV] Title: 고층 건물, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -1,22 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 건물 수
         int N = Integer.parseInt(br.readLine());
-        long[] h = new long[N];
+        int[] h = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            h[i] = Long.parseLong(st.nextToken());
+            h[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 보이는 건물 수
         int[] visible = new int[N];
 
         for (int i = 0; i < N; i++) {
-            double maxSlope = -1e18;
+            // i번째 건물에서 오른쪽 건물 -> 가장 큰 기울기
+            double maxSlope = Double.NEGATIVE_INFINITY;
 
             for (int j = i + 1; j < N; j++) {
                 double slope = (double)(h[j] - h[i]) / (j - i);
@@ -37,3 +42,11 @@ public class Main {
         System.out.println(answer);
     }
 }
+
+/*
+    보임: 두 지붕을 잇는 선분이 A와 B를 제외한 다른 건물을 지나거나 접하지 않음
+    -> 빌딩-빌딩 기울기 > 기울기
+
+
+
+ */
